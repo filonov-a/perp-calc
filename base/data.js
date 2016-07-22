4716840da020f866b6ed9dba54bef3a04396013f
Andrew Filonov <aef@bks.tv>
2016-07-22 11:57:00 +0300

    add pe and cost calculating

--- base/data.js
+++ base/data.js
@@ -66,12 +66,31 @@ function getEffByName(name, base) {
     return k * eff(x);
 }
 
+function getPEByName(name, base) {
+    var x = productivity['build'];
+    if ( prodData[name].type === 'proto' || prodData[name].type === 'protoOwn') {
+        x = productivity['buildProto'];
+    }
+    return (x+base);
+}
+
+function getTimeEffByName(name, base) {
+    var x = getPEByName(name, base);
+    var k = 1;
+    if ( prodData[name].type === 'proto' || prodData[name].type === 'protoOwn') {
+        k = 10;
+    }
+    return k * eff(x);
+}
+
 var priceData = [];
 function updatePrices() {
     productivity['base'] = (price['Эффективность переработки']);
     productivity['item'] = (price['Эффективность производства']);
     productivity['proto'] = (price['Эффективность прототипирования']);
     productivity['protoOwn'] = productivity['proto'];
+    productivity['build'] = price['Производительность производства'];
+    productivity['buildProto'] = price['Производительность прототипирования'];
 
     for (v in price) {
         priceData.push({ name: v, price: price[v] });
@@ -88,16 +107,19 @@ function editPrices() {
     updatePrices();
 }
 var categories = [];
+var itemNames = [];
 function updateCategories() {
     for (v in prodData) {
         // console.log("Price for " + v + ':' + price[v]);
         priceData.push({ name: v, price: price[v] });
     }
 }
-var categories = [];
+
 function createCategories() {
     var cat = {};
     for (var v in prodData) {
+        itemNames.push(v);
+
         if (!ownCT[v] && prodData[v].type != 'proto' && prodData[v].type != 'base') {
             continue;
         }
@@ -105,7 +127,7 @@ function createCategories() {
             continue;
         }
         if (ownCT[v]) {
-            prodData[v].cost = ownCT[v].cost || 0;
+            //prodData[v].cost = ownCT[v].cost || 0;
             prodData[v].me = ownCT[v].me || 0;
         }
         var newRec = {
@@ -151,11 +173,30 @@ function createCategories() {
 
     };
     categories.sort();
+    itemNames.sort();
+
 }
 
 
 function to_json(s) {
     return JSON.stringify(s, null, 2);
+}
+function to_time(x) {
+
+    var sec_num = Math.ceil(x);
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    if( hours > 23){
+        var days = Math.floor(hours / 24);
+        hours -= days * 24;
+        return days + "д " + hours+':'+minutes+':'+seconds;
+    }
+    return hours+':'+minutes+':'+seconds;
 }
 function to_number(x) {
     var n = Math.ceil(x);
@@ -165,3 +206,160 @@ function to_number(x) {
 }
 
 createCategories();
+
+var lastCalculatedRowId;
+function recalcTotal() {
+    var table = $$('detailTable');
+    var total = {};
+    var quantity = $$('quantity').getValue();
+    table.eachRow(
+        function (row) {
+            var v = table.getItem(row);
+            //console.log(to_json(v));
+            if (v.name == 'Цена производства') {
+                total['NIC'] = v.cost * quantity;
+            }
+            if (v.open == false) {
+
+                if (total[v.name]) {
+                    total[v.name] += v.value;
+                } else {
+                    total[v.name] = v.value;
+                }
+
+            }
+        }
+    );
+    var result = [];
+    for (v in total) {
+        result.push({ name: v, value: to_number(total[v]) });
+    }
+    $$('totalTable').clearAll();
+    $$('totalTable').define('data', result);
+}
+function recalcDetail() {
+    var itemName = $$('itemName').getValue();
+    if (itemName === '') {
+        return;
+    }
+    var table = $$('detailTable');
+    table.clearAll();
+    var baseItemMe = parseInt($$('me').getValue(), 10);
+    var quantity = parseInt($$('quantity').getValue(), 10);
+    var d = getData(itemName, quantity, baseItemMe);
+    //var icon = prodData[itemName].img;
+    //$$('itemIco').setValue("icons/"+icon+".png");
+    $$('detailTable').define('data', d);
+    recalcTotal();
+    table.eachRow(
+        function (row) {
+            var v = table.getItem(row);
+            if (v.cost && v.cost != NaN) {
+                v.costVisible = to_number(v.cost);
+                table.updateItem(row, v);
+            }
+        }
+    );
+
+    var priceTable = $$('catTable');
+    if (lastCalculatedRowId >= 0) {
+        var tableItem = priceTable.getItem(lastCalculatedRowId);
+        tableItem.rescost = d[d.length - 1].cost;
+        priceTable.updateItem(lastCalculatedRowId, tableItem);
+        table.refresh();
+    }
+}
+
+function findItem() {
+    $$('itemName').setValue($$('itemNameSelector').getValue());
+    lastCalculatedRowId = -1;
+    recalcDetail();
+}
+var priceData = [];
+
+function getMaterials(name, quantity, baseItemMe) {
+    var arr = [];
+    var materials = prodData[name].material;
+    var k = 1;
+    var me = 0;
+    var cost = prodData[name].cost || 0;
+
+    me = getProductivityByName(name, baseItemMe);
+    k = getEffByName(name, baseItemMe);
+    //console.log("Eff " + name + " " + me + ' : ' + k + " q:",quantity);
+    for (v in materials) {
+        // console.log("Found " + v + ':' + materials[v]);
+        var matValue = Math.ceil(materials[v] * quantity * ((materials[v] === 1) ? 1 : k));
+        var obj = {
+            name: v,
+            basevalue: Math.floor(materials[v] * quantity),
+            value: matValue,
+            open: false,
+            'me': getProductivityByName(v, 0)
+        };
+        if (prodData[v]) {
+            var data = getMaterials(v, materials[v] * quantity, 0);
+            obj.data = data[0];
+            obj.cost = data[1];
+        }
+        if (!obj.cost || obj.cost === NaN) {
+            if (price[obj.name]) {
+                obj.cost = Math.ceil(price[obj.name] * obj.value);
+            } else {
+                obj.cost = '???';
+            }
+        }
+        obj.costVisible = obj.cost;
+        //console.log("Price " + obj.name + " -> " +  price[obj.name]);
+        //console.log("Cost " + name + "/" + obj.name + " -> " + cost + " " + obj.cost);
+        if (obj.cost != '???') {
+            cost += obj.cost;
+        }
+        arr.push(obj);
+    }
+    //console.log("FinishCost " + name + " -> " + cost);
+    return [arr, cost];
+}
+function getData(name, quantity, baseItemMe) {
+    var arr = [];
+    var item = prodData[name];
+    var cost = item.cost || 0;
+
+    if (item.num) {
+        arr.push({ name: 'Размер партии', value: item.num });
+    }
+    var pe = getPEByName(name, baseItemMe);
+    var buildTime = item.time * getTimeEffByName(name, baseItemMe);
+    item.cost = buildTime*3;
+    arr.push({
+        name: 'Время производства',
+        me: pe,
+        basevalue:eff(pe),
+        costVisible: to_time(buildTime)
+    });
+    arr.push({ name: 'Стоимость производства', cost: item.cost });
+    var baseME = getProductivityByName(name, 0 + baseItemMe);
+    arr.push({ name: 'Эффективность КШ', me: item.me });
+    arr.push({ name: 'Эфф. Фабрики', me: getBaseProductivityByName(name, 0) });
+    arr.push({
+        name: 'Эфф. Итоговая',
+        basevalue: getEffByName(name, baseItemMe),
+        me: baseME,
+    });
+    var data = getMaterials(name, quantity, baseItemMe);
+    var materials = data[0];
+    cost = data[1];
+
+    for (v in materials) {
+        arr.push(materials[v]);
+    }
+    //console.log("getData " + to_json(arr));
+    arr.push({
+        name: 'Итого',
+        cost: cost,
+        basevalue: to_number(cost / (prodData[name].num || 1))
+    });
+    return arr;
+}
+
+updatePrices();
--- base/main.js
+++ base/main.js
@@ -2,148 +2,30 @@
 
 //console.log(to_json(price));
 //console.log(to_json(prodData));
-var lastCalculatedRowId;
-function recalcTotal() {
-    var table = $$('detailTable');
-    var total = {};
-    var quantity = $$('quantity').getValue();
-    table.eachRow(
-        function (row) {
-            var v = table.getItem(row);
-            //console.log(to_json(v));
-            if (v.name == 'Цена производства') {
-                total['NIC'] = v.cost * quantity;
-            }
-            if (v.open == false) {
 
-                if (total[v.name]) {
-                    total[v.name] += v.value;
-                } else {
-                    total[v.name] = v.value;
-                }
-
-            }
-        }
-    );
-    var result = [];
-    for (v in total) {
-        result.push({ name: v, value: to_number(total[v]) });
-    }
-    $$('totalTable').clearAll();
-    $$('totalTable').define('data', result);
-}
-function recalcDetail() {
-    var itemName = $$('itemName').getValue();
-    var table = $$('detailTable');
-    table.clearAll();
-    var baseItemMe = parseInt($$('me').getValue(), 10);
-    var quantity = parseInt($$('quantity').getValue(), 10);
-    var d = getData(itemName, quantity, baseItemMe);
-    $$('detailTable').define('data', d);
-    recalcTotal();
-    table.eachRow(
-        function (row) {
-            var v = table.getItem(row);
-            if (v.cost && v.cost != NaN) {
-                v.costVisible = to_number(v.cost);
-                table.updateItem(row, v);
-            }
-        }
-    );
-    var priceTable = $$('catTable');
-
-    var tableItem = priceTable.getItem(lastCalculatedRowId);
-    tableItem.rescost = d[d.length -1].cost;
-    priceTable.updateItem(lastCalculatedRowId, tableItem);
-    table.refresh();
-}
-var priceData = [];
-
-function getMaterials(name, quantity, baseItemMe) {
-    var arr = [];
-    var materials = prodData[name].material;
-    var k = 1;
-    var me = 0;
-    var cost = prodData[name].cost || 0;
-
-    me = getProductivityByName(name, baseItemMe);
-    k = getEffByName(name, baseItemMe);
-    //console.log("Eff " + name + " " + me + ' : ' + k + " q:",quantity);
-    for (v in materials) {
-        // console.log("Found " + v + ':' + materials[v]);
-        var matValue = Math.ceil(materials[v] * quantity * (( materials[v] === 1)?1 : k));
-        var obj = {
-            name: v,
-            basevalue: Math.floor(materials[v] * quantity),
-            value: matValue,
-            open: false,
-            'me': getProductivityByName(v, 0)
-        };
-        if (prodData[v]) {
-            var data = getMaterials(v, materials[v] * quantity , 0);
-            obj.data = data[0];
-            obj.cost = data[1];
-        }
-        if (!obj.cost || obj.cost === NaN) {
-            if (price[obj.name]) {
-                obj.cost = Math.ceil(price[obj.name] * obj.value);
-            } else {
-                obj.cost = '???';
-            }
-        }
-        obj.costVisible = obj.cost;
-        //console.log("Price " + obj.name + " -> " +  price[obj.name]);
-        //console.log("Cost " + name + "/" + obj.name + " -> " + cost + " " + obj.cost);
-        if (obj.cost != '???') {
-            cost += obj.cost;
-        }
-        arr.push(obj);
-    }
-    //console.log("FinishCost " + name + " -> " + cost);
-    return [arr, cost];
-}
-function getData(name, quantity, baseItemMe) {
-    var arr = [];
-    var item = prodData[name];
-    var cost = item.cost||0;
-    if (item.num) {
-        arr.push({ name: 'Размер партии', value: item.num });
-    }
-    arr.push({ name: 'Цена производства', cost: item.cost });
-    var baseME = getProductivityByName(name, 0 + baseItemMe);
-    arr.push({ name: 'Эффективность КШ', me: item.me });
-    arr.push({ name: 'Эфф. Фабрики', me: getBaseProductivityByName(name, 0) });
-    arr.push({
-        name: 'Эфф. Итоговая',
-        basevalue: getEffByName(name, baseItemMe),
-        me: baseME,
-    });
-    var data = getMaterials(name, quantity, baseItemMe);
-    var materials = data[0];
-    cost = data[1];
-
-    for (v in materials) {
-        arr.push(materials[v]);
-    }
-    //console.log("getData " + to_json(arr));
-    arr.push({
-        name: 'Итого',
-        cost: cost,
-        basevalue: to_number(cost / (prodData[name].num || 1))
-    });
-    return arr;
-}
-
-updatePrices();
-var info = to_json(categories);
+//var info = to_json(categories);
 var detailTable = {
     type: "space",
     autoheight: true,
     id: 'detailView',
     rows: [
         {
+            height: 40,
             cols: [
-                { view: "label", id: "itemName", align: "left", },
+                /*                {
+                                    view: "template", id: "itemIco", align: "left",
+                                    width: 40,
+                                    data: {
+                                        src: "icons/noIconAvailable.png",
+                                        desr: "Icon"
+                                    },
+                                    template: function (obj) {
+                                        return '<img src="' + obj.src + '" width=32 height=32 align=top >';
+                                    }
+                                },
+                */
+                { view: "label", id: "itemName", align: "left" },
+
                 {
                     view: "text",
                     id: "quantity",
@@ -162,7 +44,21 @@ var detailTable = {
                     click: recalcDetail
 
                 },
-            ]
+                {
+                    view: "combo",
+                    width: 450,
+                    id: "itemNameSelector",
+                    options: itemNames,
+                    //label: "Поиск",
+                },
+                 {
+                    view: "button",
+                    label: "Поиск",
+                    width: 100,
+                    click: findItem,
+
+                },
+                ]
         },
         {
 
